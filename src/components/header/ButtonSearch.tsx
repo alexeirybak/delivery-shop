6eb5dc24a489ae6ff683e3110fc1fb4629c1c655
Алексей Рыбак <rybak2108@yandex.ru>
2025-06-28 18:52:20 +0300

    checked

--- src/components/header/ButtonSearch.tsx
+++ src/components/header/ButtonSearch.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const ButtonSearch = () => {
   return (
-    <button className="bg-(--color-primary) hover:shadow-(--shadow-button-default) active:shadow-(--shadow-button-active) hidden md:flex w-10 p-2 gap-4 lg:w-35 cursor-pointer duration-300 rounded">
+    <div className="bg-(--color-primary) hover:shadow-(--shadow-button-default) active:shadow-(--shadow-button-active) hidden md:flex w-10 p-2 gap-4 lg:w-35 cursor-pointer duration-300 rounded">
       <Image
         src="/icons-header/icon-menu.svg"
         alt="menu"
@@ -10,8 +11,10 @@ const ButtonSearch = () => {
         height={24}
         className="hidden md:block"
       />
-      <span className="text-base text-white hidden lg:block">Каталог</span>
-    </button>
+      <Link href="./catalog" className="text-base text-white hidden lg:block">
+        Каталог
+      </Link>
+    </div>
   );
 };
 
