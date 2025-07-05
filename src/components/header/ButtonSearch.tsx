c6aceac9e36fdc0986cc41a0e2bfbf75c806b766
alexeirybak <128369349+alexeirybak@users.noreply.github.com>
2025-07-05 13:28:45 +0300

    Update ButtonSearch.tsx

--- src/components/header/ButtonSearch.tsx
+++ src/components/header/ButtonSearch.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const ButtonSearch = () => {
   return (
-    <Link href='./catalog' className="bg-(--color-primary) hover:shadow-(--shadow-button-default) active:shadow-(--shadow-button-active) hidden md:flex w-10 p-2 gap-4 lg:w-35 cursor-pointer duration-300 rounded">
+    <Link href='../catalog' className="bg-(--color-primary) hover:shadow-(--shadow-button-default) active:shadow-(--shadow-button-active) hidden md:flex w-10 p-2 gap-4 lg:w-35 cursor-pointer duration-300 rounded">
       <Image
         src="/icons-header/icon-menu.svg"
         alt="menu"
