81d2a6623748960e2d7b7b4a67eb1973974bbee6
alexeirybak <rybak2108@yandex.ru>
2025-07-10 00:04:15 +0300

    checked

--- src/types/paginationProps.ts
+++ src/types/paginationProps.ts
@@ -3,9 +3,5 @@ export interface PaginationProps {
   currentPage: number;
   basePath: string;
   itemsPerPage: number;
-  searchParams: { 
-    page?: string; 
-    itemsPerPage?: string 
-  };
   searchQuery: string;
 }
