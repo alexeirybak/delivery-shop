38a1c49c599a4b89bd3ac355bb00ff2a764b4472
Алексей Рыбак <rybak2108@yandex.ru>
2025-08-21 11:24:37 +0300

    checked

--- src/app/(auth)/(login)/login/page.tsx
+++ src/app/(auth)/(login)/login/page.tsx
@@ -37,6 +37,16 @@ const EnterLoginPage = () => {
     setError(null);
   };
 
+  const handleForgotPassword = () => {
+    if (loginType === "phone") {
+      router.replace(
+        `/phone-pass-reset`
+      );
+    } else {
+      router.replace("/forgot-password");
+    }
+  };
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setIsLoading(true);
@@ -245,12 +255,12 @@ const EnterLoginPage = () => {
           >
             Регистрация
           </Link>
-          <Link
-            href="/forgot-password"
-            className="h-8 text-[#414141] hover:text-black w-30 flex items-center justify-center duration-300"
+          <button
+            onClick={handleForgotPassword}
+            className="h-8 text-[#414141] hover:text-black w-30 flex items-center justify-center duration-300 cursor-pointer"
           >
             Забыли пароль?
-          </Link>
+          </button>
         </div>
       </form>
     </AuthFormLayout>
