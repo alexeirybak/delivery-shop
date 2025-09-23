9d74fbc2bcce3543007ddee1db2e0e1196ed9d05
Алексей Рыбак <rybak2108@yandex.ru>
2025-09-23 20:37:38 +0300

    checked

--- src/components/ProductsSection.tsx
+++ src/components/ProductsSection.tsx
@@ -8,6 +8,7 @@ const ProductsSection = ({
   products,
   applyIndexStyles = true,
   contentType,
+  mobileItemsLimit = 4,
 }: ProductsSectionProps & {
   applyIndexStyles?: boolean;
   contentType?: string;
@@ -40,9 +41,9 @@ const ProductsSection = ({
                 key={item._id}
                 className={
                   applyIndexStyles
-                    ? index >= 3
-                      ? "md:hidden xl:block"
-                      : ""
+                    ? `${index >= mobileItemsLimit ? "hidden md:block" : ""}
+                    ${index >= 3 ? "md:hidden xl:block" : ""}
+                    ${index >= 4 ? "xl:hidden" : ""}`
                     : ""
                 }
               >
