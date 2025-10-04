9d2ceb9ba8b133cf09d094c6f6a1f60c6edfe59e
alexeirybak <128369349+alexeirybak@users.noreply.github.com>
2025-10-05 01:02:03 +0300

    Update route.ts

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -4,7 +4,6 @@ import { getOrderCartAction } from "@/actions/orderActions";
 export async function GET() {
   try {
     const cartItems = await getOrderCartAction();
-    console.log("API cart items:", cartItems); // Добавляем лог
     return NextResponse.json(cartItems);
   } catch (error) {
     console.error("Error in cart API:", error);
