b2f68b15dac421d8addb6325d492ad168dd4d27c
Алексей Рыбак <rybak2108@yandex.ru>
2026-02-04 21:56:08 +0300

    checked

--- src/app/(admin)/administrator/(cms)/cms/utils/getStatValue.ts
+++ src/app/(admin)/administrator/(cms)/cms/utils/getStatValue.ts
@@ -1,7 +1,9 @@
 export const getStatValue = (
   statTitle: string,
   categoriesCount: string,
-  keywordsCount: string
+  keywordsCount: string,
+  publishedCount: string,
+  viewsCount: string,
 ) => {
   switch (statTitle) {
     case "Категорий":
@@ -9,9 +11,9 @@ export const getStatValue = (
     case "Ключевых слов":
       return keywordsCount;
     case "Опубликовано":
-      return "0";
+      return publishedCount;
     case "Просмотров":
-      return "0";
+      return viewsCount;
     default:
       return "0";
   }
